f308d3895ef0d3908fcd12dacfa734f817424136
corbotarik <64921481+corbotarik@users.noreply.github.com>
2020-09-29 10:39:23 +0200

    Add files via upload

--- hachimenu/config.h
+++ hachimenu/config.h
@@ -4,13 +4,13 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=14"
+	"monospace:size=12"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#303030" },/* letter color - menu color*/
-	[SchemeSel] = { "#303030", "#94ebeb" },/* letter selected color - select color*/
+	[SchemeSel] = { "#303030", "#4771b5" },/* letter selected color - select color*/
 	[SchemeOut] = { "#f20202", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
